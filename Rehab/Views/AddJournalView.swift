1e3fa2274af0396050ba8934e423c13958fdbd3d
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-03-15 16:13:05 -0700

    create Journal Entity in Core Data

---
+++ Rehab/Views/AddJournalView.swift
@@ -0,0 +1,20 @@
+//
+//  AddJournalView.swift
+//  Rehab
+//
+//  Created by Joshua Basche on 3/15/21.
+//
+
+import SwiftUI
+
+struct AddJournalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddJournalView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddJournalView()
+    }
+}
