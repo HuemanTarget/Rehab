b828aa1d9f8f98741f3969bb6d39936baf796ef7
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-04-12 21:20:17 -0700

    create disclaimer view

---
+++ Rehab/Views/Disclaimer/DisclaimerView.swift
@@ -0,0 +1,26 @@
+//
+//  DisclaimerView.swift
+//  Rehab
+//
+//  Created by Joshua Basche on 4/12/21.
+//
+
+import SwiftUI
+
+struct DisclaimerView: View {
+    var body: some View {
+      VStack {
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+            .padding(40)
+          .font(.title3)
+        
+        Spacer()
+      }
+    }
+}
+
+struct DisclaimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        DisclaimerView()
+    }
+}
